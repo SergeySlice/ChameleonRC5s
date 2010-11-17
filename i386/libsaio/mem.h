c33486a0b9ea1cb12389c784bdb0ab95aa9229c9
SergeySlice <isakov-sl@bk.ru>
2010-11-17 17:47:49 +0000

    Initial

---
+++ i386/libsaio/mem.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2010 AsereBLN. All rights reserved. <aserebln@googlemail.com>
+ *
+ * mem.h
+ */
+
+#ifndef __LIBSAIO_MEM_H
+#define __LIBSAIO_MEM_H
+
+#include "platform.h"
+
+extern void scan_memory(PlatformInfo_t *);
+extern void scan_cpu_DMI(PlatformInfo_t *); //Slice
+
+
+#endif	/* __LIBSAIO_MEM_H */
